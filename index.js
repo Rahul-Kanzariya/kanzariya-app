c47ddcc347d3728ed8755acf39960c0b33da8851
rahul-kanzariya <kanzariyarahulbhai@gmail.com>
2023-05-04 20:04:12 +0530

    temp changes

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function hasClass(ele, cls) {
 }
 
 function addClass(ele, cls) {
-    if (!hasClass(ele, cls)) ele.className += " " + cls;
+    if (!hasClass(ele, cls)) ele.className += "" + cls;
 }
 
 function removeClass(ele, cls) {
     if (hasClass(ele, cls)) {
         var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-        ele.className = ele.className.replace(reg, ' ');
+        ele.className = ele.className.replace(reg, '');
     }
 }
 
