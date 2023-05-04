2ddbfd94ad5ce26496b526ef4ce4a5a94b12d46a
rahul-kanzariya <kanzariyarahulbhai@gmail.com>
2023-05-04 18:57:52 +0530

    logger

--- kanzariya-app/emplyee-card/employee-card.js
+++ kanzariya-app/emplyee-card/employee-card.js
@@ -2,10 +2,10 @@ export default class EmployeeCard extends HTMLElement {
     template;
     constructor() {
       super();
-      this.createTemplate();
-      this.attachShadow({ mode: 'open'});
-      this.shadowRoot.appendChild(this.template.content.cloneNode(true));
-      this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
+      // this.createTemplate();
+      // this.attachShadow({ mode: 'open'});
+      // this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+      // this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
     }
     
     createTemplate(){
@@ -40,12 +40,38 @@ export default class EmployeeCard extends HTMLElement {
     }
 
     connectedCallback(){
-        this.h3 = this.getAttribute("name");
+        // this.h3 = this.getAttribute("name");
+        this.innerHTML = `
+        <style>
+        <style>
+          .employee-card {
+            font-family: sans-serif;
+            background: #f4f6f7;
+            width: 250px;
+            display: grid;
+            grid-template-columns: 1fr;
+            margin-bottom: 10px;
+          }
+        
+        </style>
+        <div class="employee-card">
+          <img/>
+          <div>
+            <h3 class="card-name"></h3>
+            <div class="details">
+              <p><slot name="id"/></p>
+              <p><slot name="job title"/></p>
+              <p><slot name="email"/></p>
+              <p><slot name="phone"/></p>
+            </div>
+          </div>
+        </div>
+        `
         this.render();
     }
     
     render(){
-        this.h3
+      this.querySelector('h3').innerText = this.getAttribute('name');
     }
   }
 
