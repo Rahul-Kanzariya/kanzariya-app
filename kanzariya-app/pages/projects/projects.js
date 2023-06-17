96072f6b55d991b5966b00059a966c7e312690ec
rahul-kanzariya <kanzariyarahulbhai@gmail.com>
2023-06-17 18:37:00 +0530

    audio upload and play

--- kanzariya-app/pages/projects/projects.js
+++ kanzariya-app/pages/projects/projects.js
@@ -16,6 +16,10 @@ export default class Projects extends HTMLElement {
       this.template.innerHTML = template; 
       this.attachShadow({ mode: 'open'});
       this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+
+      const uploadedAudio = this.shadowRoot.getElementById("audio-upload");
+      // console.log('uploadedAudio: ', uploadedAudio);
+      if(uploadedAudio)uploadedAudio.addEventListener("change", this.changeHandler);
     }
 
     connectedCallback(){
@@ -26,4 +30,32 @@ export default class Projects extends HTMLElement {
     render(){
       // this.querySelector('h3').innerText = this.getAttribute('name');
     }
+
+    changeHandler({
+      target
+    }) {
+      // console.log("changeHandler : target >>>", target);
+      // Make sure we have files to use
+      if (!target.files.length) return;
+    
+      // Create a blob that we can use as an src for our audio element
+      const urlObj = URL.createObjectURL(target.files[0]);
+    
+      // Create an audio element
+      const audio = document.createElement("audio");
+    
+      // Clean up the URL Object after we are done with it
+      audio.addEventListener("load", () => {
+        URL.revokeObjectURL(urlObj);
+      });
+    
+      // Append the audio element
+      document.body.appendChild(audio);
+    
+      // Allow us to control the audio
+      audio.controls = "true";
+    
+      // Set the src and start loading the audio from the file
+      audio.src = urlObj;
+    } 
   }
